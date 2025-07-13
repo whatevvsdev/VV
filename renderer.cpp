48c40541d1ee9a34531099355da3c406f94767df
Matt Kaločai <212246@buas.nl>
2025-07-13 22:10:21 +0200

    Getting the physical device, creating a logical device.

--- renderer.cpp
+++ renderer.cpp
@@ -1,14 +1,19 @@
 ﻿#include "renderer.h"
 #include <cstdio>
+#include <vector>
+#include "types.h"
 
 #include <Vulkan/Vulkan.h>
 
 struct
 {
     VkInstance instance { VK_NULL_HANDLE };
+    VkPhysicalDevice physical_device { VK_NULL_HANDLE };
+    VkDevice device { VK_NULL_HANDLE };
+    u32 graphics_and_compute_queue_family_index { 0 };
 } core;
 
-bool init_vulkan_instance()
+bool create_vulkan_instance()
 {
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -28,18 +33,118 @@ bool init_vulkan_instance()
 
     if (!created_instance)
     {
-        printf("Failed to initialize Vulkan instance.\n");
+        printf("Failed to create Vulkan instance.\n");
         core.instance = VK_NULL_HANDLE;
         return false;
     }
 
-    printf("Initialized Vulkan instance.\n");
+    printf("Created Vulkan instance.\n");
+    return true;
+}
+
+bool pick_vulkan_physical_device()
+{
+    // Get count of physical devices available
+    u32 physical_device_count { 0 };
+    VkResult result = vkEnumeratePhysicalDevices(core.instance, &physical_device_count, nullptr);
+
+    // Get all physical devices available
+    VkPhysicalDevice physical_devices[physical_device_count];
+    vkEnumeratePhysicalDevices(core.instance, &physical_device_count, physical_devices);
+
+    struct DeviceProperties
+    {
+        VkPhysicalDeviceProperties properties;
+        VkPhysicalDeviceProperties2 properties2;
+    };
+
+    // Get the properties of all devices
+    std::vector<DeviceProperties> device_properties(physical_device_count);
+
+    for (u32 i = 0; i < physical_device_count; i++)
+    {
+        device_properties[i].properties2.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2;
+        device_properties[i].properties2.pNext = &device_properties[i].properties;
+        vkGetPhysicalDeviceProperties2(physical_devices[i], &device_properties[i].properties2);
+    }
+
+    if (physical_device_count == 0)
+    {
+        printf("Failed to find any physical devices.\n");
+        return false;
+    }
+
+    // Get the properties of each queue family available on each physical device
+    for (u32 i = 0; i < physical_device_count; i++)
+    {
+        // Get queue family count
+        u32 queue_family_count { 0 };
+        vkGetPhysicalDeviceQueueFamilyProperties(physical_devices[i], &queue_family_count, nullptr);
+
+        // Get queue family properties
+        std::vector<VkQueueFamilyProperties> queue_family_properties(queue_family_count);
+        vkGetPhysicalDeviceQueueFamilyProperties(physical_devices[i], &queue_family_count, queue_family_properties.data());
+
+        // Find a suitable graphics card
+        for (u32 f = 0; f < queue_family_count; f++)
+        {
+            VkQueueFlags& flags = queue_family_properties[f].queueFlags;
+            // Transfer queue is implicitly valid thanks to graphics/compute
+            if ((flags & VK_QUEUE_GRAPHICS_BIT) && (flags & VK_QUEUE_COMPUTE_BIT))
+            {
+                core.physical_device = physical_devices[i];
+                core.graphics_and_compute_queue_family_index = f;
+                printf("Found and picked device with name: %s\n", device_properties[i].properties2.properties.deviceName);
+                break;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool create_vulkan_device()
+{
+    VkDeviceCreateInfo create_info{};
+    create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    create_info.pNext = nullptr;
+    create_info.flags = 0; // Reserved
+
+    float queue_priority = 1.0f;
+    std::vector<VkDeviceQueueCreateInfo> queue_create_infos(1);
+    queue_create_infos[0].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queue_create_infos[0].pNext = nullptr;
+    queue_create_infos[0].flags = 0;
+    queue_create_infos[0].queueFamilyIndex = core.graphics_and_compute_queue_family_index;
+    queue_create_infos[0].queueCount = 1;
+    queue_create_infos[0].pQueuePriorities = &queue_priority;
+
+    create_info.queueCreateInfoCount = queue_create_infos.size();
+    create_info.pQueueCreateInfos = queue_create_infos.data();
+
+    create_info.enabledLayerCount = 0; // Deprecated
+    create_info.ppEnabledLayerNames = nullptr; // Deprecated
+    create_info.enabledExtensionCount = 0;
+    create_info.ppEnabledExtensionNames = nullptr;
+    create_info.pEnabledFeatures = nullptr; // Deprecated
+
+    bool created_device = vkCreateDevice(core.physical_device, &create_info, nullptr, &core.device) == VK_SUCCESS;
+
+    if (!created_device)
+    {
+        printf("Failed to create Vulkan device.\n");
+        core.device = VK_NULL_HANDLE;
+        return false;
+    }
+
     return true;
 }
 
 void Renderer::initialize()
 {
-    init_vulkan_instance();
+    create_vulkan_instance();
+    pick_vulkan_physical_device();
+    create_vulkan_device();
 }
 
 void Renderer::update()
@@ -50,9 +155,6 @@ void Renderer::update()
 void Renderer::terminate()
 {
     // TODO: Replace this with a deletion queue
-    if (core.instance != VK_NULL_HANDLE)
-    {
-        vkDestroyInstance(core.instance, nullptr);
-    }
+    vkDestroyInstance(core.instance, nullptr);
 }
 
---
+++ types.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <cstdint>
+
+typedef uint8_t		u8;
+typedef uint16_t	u16;
+typedef uint32_t	u32;
+typedef uint64_t	u64;
+typedef size_t      usize;
+
+static_assert(sizeof(u8) == 1,  "u8 type is not 1 byte.");
+static_assert(sizeof(u16) == 2, "u16 type is not 2 bytes.");
+static_assert(sizeof(u32) == 4, "u32 type is not 4 bytes.");
+static_assert(sizeof(u64) == 8, "u64 type is not 8 bytes.");
+
+typedef int8_t		i8;
+typedef int16_t		i16;
+typedef int32_t		i32;
+typedef int64_t		i64;
+
+static_assert(sizeof(i8) == 1,  "i8 type is not 1 byte.");
+static_assert(sizeof(i16) == 2, "i16 type is not 2 bytes.");
+static_assert(sizeof(i32) == 4, "i32 type is not 4 bytes.");
+static_assert(sizeof(i64) == 8, "i64 type is not 8 bytes.");
+
+typedef float		f32;
+typedef double		f64;
+
+static_assert(sizeof(f32) == 4, "f32 type is not 4 byte.");
+static_assert(sizeof(f64) == 8, "f64 type is not 8 bytes.");
