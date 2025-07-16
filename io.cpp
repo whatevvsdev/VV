91113febb9dad20c6a47fc6546196106e341779c
Matt Kaločai <212246@buas.nl>
2025-07-16 15:54:50 +0200

    Added IO for reading of binary files.

---
+++ io.cpp
@@ -0,0 +1,23 @@
+﻿#include "io.h"
+#include <fstream>
+
+namespace IO
+{
+    std::vector<u8> read_binary_file(const FSPath& path)
+    {
+        std::vector<u8> buffer(0);
+        std::ifstream file(path, std::ios::ate | std::ios::binary);
+
+        if (!file.is_open())
+        {
+            printf("Failed to read file %ls.\n", path.c_str());
+            return buffer;
+        }
+
+        buffer.resize(file.tellg());
+        file.seekg(0, std::ios::beg);
+        file.read(reinterpret_cast<std::fstream::char_type*>(buffer.data()), static_cast<std::streamsize>(buffer.size()));
+
+        return buffer;
+    }
+}
---
+++ io.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <filesystem>
+#include <vector>
+#include "types.h"
+
+typedef std::filesystem::path FSPath;
+
+namespace IO
+{
+    std::vector<u8> read_binary_file(const FSPath& path);
+}
