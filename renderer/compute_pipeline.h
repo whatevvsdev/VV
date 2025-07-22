41d2aea258b7a91476094632447bbd5ffd28df4e
Matt Kaločai <212246@buas.nl>
2025-07-22 21:45:49 +0200

    Start of compute pipeline abstraction

---
+++ renderer/compute_pipeline.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "vv_vulkan.h"
+#include "vk_mem_alloc.h"
+
+struct ComputePipelineBuilder
+{
+
+};
+
+struct ComputePipeline
+{
+    VkBuffer descriptor_buffer { VK_NULL_HANDLE };
+    VmaAllocation descriptor_buffer_allocation { VK_NULL_HANDLE };
+};
